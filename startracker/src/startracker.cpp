6ba6deaa48af66eb737f095e72ca68d2fb5a8eaa
lalagi <tothlajosg@gmail.com>
2013-03-06 16:33:18 +0100

    das

--- startracker/src/startracker.cpp
+++ startracker/src/startracker.cpp
@@ -4,27 +4,74 @@
 using namespace cv;
 using namespace std;
 
-const int noiseTreshold = 16;
-const int starTreshold = 195;
-const int roiSize = 11;
+int gnoiseTreshold = 0;
+int gstarTreshold = 60;
+double unit = 1;
+const int groiSize = 11;
+const double atlagFaktor = 3.8; // megfelelo zajkuszob kiszamitasahoz hasznalt suly
+const double csillagFaktor = 3; // megfelelo csillagkuszob kiszamitasahoz hasznalt suly
 
-Mat_<uchar> preFiltered;
-
-Point centers[30];
+Point centers[300];
 int centerdb = 0;
 
-Mat preBinarizing ( Mat_<uchar> image ) {
+double root(double x){
+    double a,b;
+    b     = x;
+    a = x = 0x3f;
+    x     = b/x;
+    a = x = (x+a) / 2;
+    x     = b/x;
+    a = x = (x+a) / 2;
+    x     = b/x;
+    x     = (x+a) / 2;
+
+    return(x);
+}
+
+//zajkuszob megallapitasa
+int calculateNoiseTreshold(Mat_<uchar> image) {
+	int atlag = (int)mean(image).val[0];
+
+	cout << "Noise treshold: " << atlag << " (atlagFaktorral sulyozott eredmeny)" << endl;
+	return (int)atlag * atlagFaktor;
+}
+
+//csillagkuszob meghatarozasa
+int calculateStarTreshold(Mat_<uchar> image) {
+	double max;
+
+	minMaxIdx(image, NULL, &max); // maximalis pixelintenzitas meghatarozasa
+
+	cout << "Star treshold: " << (int)max / csillagFaktor << " (csillagFaktorral sulyozott eredmeny)" << endl;
+	return (int)max / csillagFaktor;
+}
+
+Mat preBinarizing (Mat_<uchar> image, const int noiseTreshold) {
 	Mat_<uchar> filter;
 	Mat_<uchar> out;
 
-	//ami 18nal kisebb eldobjuk
+	//ami noiseTresholdnal kisebb eldobjuk
 	compare(image, noiseTreshold, filter, CV_CMP_GT);
 	bitwise_and( image, filter, out );
 
 	return out;
 }
 
-void centerOfMassCalculation(Point2d& center, int rowvalue, int columnvalue) {
+double calculateFocalDistance(int row, int col, double fov) {
+	double angle = 180 - 90 - fov; // a 3szog 3. szoge
+	double a; // a 3szog egy oldala
+	double focalDistance;
+
+//	egyik oldala
+	a = sqrt(row / 2 * row / 2 + col / 2 * col / 2);
+
+//	sin tetel
+	focalDistance = sin(angle) * a / sin(fov / 2);
+	cout << " focal distance: " << focalDistance;
+	return focalDistance;
+}
+
+void centerOfMassCalculation(Mat_<uchar> image, Point2d& center, int rowValue, int columnValue) {
 	Mat_<uchar> imageROI;
 	Scalar dn;
 
@@ -32,93 +79,118 @@ void centerOfMassCalculation(Point2d& center, int rowvalue, int columnvalue) {
 	center.y = 0;
 
 	//calculate ROI brightness
-	imageROI = preFiltered(Range(rowvalue - roiSize, rowvalue + roiSize), Range(columnvalue - roiSize, columnvalue + roiSize));
+	imageROI = image(Range(rowValue - groiSize, rowValue + groiSize), Range(columnValue - groiSize, columnValue + groiSize));
 	dn = sum(imageROI);
 
-	for (int row = rowvalue - roiSize; row < rowvalue + roiSize + 1; row++) {
-		for (int col = columnvalue - roiSize; col < columnvalue + roiSize + 1; col++) {
-			center.x += col * (int)preFiltered(row, col);
-		}
-	}
+	Moments mom = moments(imageROI, false);
 
-	for (int row = rowvalue - roiSize; row < rowvalue + roiSize + 1; row++) {
-		for (int col = columnvalue - roiSize; col < columnvalue + roiSize + 1; col++) {
-			center.y += row * (int)preFiltered(row, col);
-		}
-	}
-
-	center.x /= dn.val[0];
-	center.y /= dn.val[0];
-
-//	center.x += 0.5;
-//	center.y += 0.5;
-
-//	cout << center.x << " " << center.y << endl;
+	center.x = (double)(mom.m10 / mom.m00) + columnValue - groiSize;
+	center.y = (double)(mom.m01 / mom.m00) + rowValue - groiSize;
 }
 
 void centroiding( Mat_<uchar> image ) {
-	bool isChecked[image.rows][image.cols];
+	Mat isChecked = Mat::zeros(image.rows, image.cols, CV_8UC1);
+	Mat mask = Mat::ones(groiSize * 2, groiSize * 2, CV_8UC1);
 	Point2d center(0, 0);
 	int db = 0;
-
-	//egyik pixelt sem neztuk meg, hogy nagyobb-e, mint a starTreshold kuszob
-	for (int row = 0; row < image.rows; row++) {
-		for (int col = 0; col < image.cols; col++) {
-			isChecked[row][col] = false;
-		}
-	}
 
 	//csillag kutatasa pixelenkent
 	for (int col = 0; col < image.cols; col++) {
 		for (int row = 0; row < image.rows; row++) {
 			//ha csillagot talaltunk && meg nem ellenoriztuk / az adott pixel meg nem volt benne egy ROIban
-			if ( (image(row, col) > starTreshold) && (isChecked[row][col] == false) ) {
+			if ( (image(row, col) > gstarTreshold) && (isChecked.at<uchar>(row, col) == (uchar)0) ) {
 				//TALALAT, ha nem log ki a csillag ROI-ja a kepbol
-				if (row - roiSize > 0 && col - roiSize > 0 && row + roiSize < 600 && col + roiSize < 600) {
-					centerOfMassCalculation(center, row, col);
+				if (row - groiSize > 0 && col - groiSize > 0 && row + groiSize < image.rows && col + groiSize < image.cols) {
+					centerOfMassCalculation(image, center, row, col);
 					centers[centerdb] = center;
 					centerdb++;
 
+					//ROI pixeleit az ischeckedben atallitjuk -> tobbet nem nezzuk meg ezeket a pixeleket
+					Mat imageROI = isChecked(Range(row - groiSize, row + groiSize), Range(col - groiSize, col + groiSize));
+					mask.copyTo(imageROI);
+
 					db++;
-					//ROI pixeleit az ischeckedben truera allitjuk -> tobbet nem nezzuk meg ezeket a pixeleket
-					for (int k = 0; k < roiSize; k++) {
-						for (int l = 0; l < roiSize; l++) {
-							isChecked[row - k][col + l] = true;
-							isChecked[row + k][col - l] = true;
-							isChecked[row - k][col - l] = true;
-							isChecked[row + k][col + l] = true;
-						}
-					}
 				}
 			}
-			isChecked[row][col] = true;
 		}
 	}
-	cout << db;
+	cout << "Ennyi csillagot detektaltunk a kepen: " << db << endl;;
+}
+
+//2 pont kozti tavolsag
+double length(Point2d p1, Point2d p2) {
+	double a = p2.x - p1.x;
+	double b = p2.y - p1.y;
+	//sqrt hanyagolando lesz beagyazottnal
+	return sqrt(a * a + b * b);
+}
+
+void searchingPattern(double area, double polarmom) {
+	if (fabs(12506 - area) < 0.0001) cout << "jee";
+}
+
+void calculateArea () {
+	double area;
+	double polarmom;
+	double s;
+	double a;
+	double b;
+	double c;
+
+	for (int csp1 = 0; csp1 < centerdb - 2; csp1++) {
+		for (int csp2 = csp1 + 1; csp2 < centerdb - 1; csp2++) {
+			for (int csp3 = csp1 + 2; csp3 < centerdb; csp3++) {
+				a = length(centers[csp2], centers[csp1]);
+				b = length(centers[csp3], centers[csp2]);
+				c = length(centers[csp1], centers[csp3]);
+
+				s = 0.5 * (a + b + c);
+				//sqrt hanyagolando lesz beagyazottnal
+				area = sqrt(s * (s - a) * (s - b) * (s - c));
+				polarmom = area * (a * a + b * b + c * c) / 36;
+
+				searchingPattern(area, polarmom);
+//				cout << a << " " << b << " " << c << " " << area << " " << polarmom << endl;
+//				cout << centers[csp2] - centers[csp1] << endl;
+			}
+		}
+	}
 }
 
 int main( int argc, char** argv ) {
 	//kep betoltese
 	Mat input = imread( argv[1] );
+	Mat_<uchar> preFiltered;
 
-//	namedWindow("in");
-//	imshow("in", input);
+	namedWindow("in");
+	imshow("in", input);
 
 	//rgb to gray
 	cvtColor( input, preFiltered, CV_RGB2GRAY );
 
-	//eloszures
-	preFiltered = preBinarizing( preFiltered );
+	//kuszobok kiszamolasa
+	gnoiseTreshold = calculateNoiseTreshold(preFiltered);
+	gstarTreshold = calculateStarTreshold(preFiltered);
 
+	//eloszures
+	preFiltered = preBinarizing( preFiltered, gnoiseTreshold );
 	centroiding(preFiltered);
 
+	namedWindow( "out", CV_WINDOW_AUTOSIZE );
+	//terulet kiszamolasa
+	calculateArea();
+
 	for (int i = 0; i < centerdb; i++) {
+		stringstream ss;
+		ss << i;
+		string str = ss.str();
+		putText(preFiltered, str, centers[i], FONT_HERSHEY_SIMPLEX, 0.5, cvScalar(200,200,250), 0.5, CV_AA);
 		circle(preFiltered, centers[i], 7, Scalar(255));
 	}
 
-	namedWindow( "out", CV_WINDOW_AUTOSIZE );
-	imshow( "out", preFiltered );
+	calculateFocalDistance(preFiltered.rows, preFiltered.cols, 36);
 
+	imshow( "out", preFiltered );
 	waitKey(0);
 
 	return 0;
